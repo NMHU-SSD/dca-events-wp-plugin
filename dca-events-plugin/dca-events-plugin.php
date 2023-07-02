eb8635b96555d501a3854ee4372f91b1c84edb17
Anita Martin <50088347+anita-martin5703@users.noreply.github.com>
2023-07-02 14:19:35 -0600

    Added in a few changes
    Added in a few changes

--- dca-events-plugin/dca-events-plugin.php
+++ dca-events-plugin/dca-events-plugin.php
@@ -29,12 +29,18 @@ function dca_events_plugin($atts = [])
 	//gets shortcode vals
 	$atts = shortcode_atts(
 		array(
-			'id' => NULL, //site id for museum
-			'current-day' => false, //events by current day
-			'current-month' => false, //events by current month
-			'date-range' => false, //events by range
-			'range-start' => NULL, //start range
-			'range-end' => NULL, //end range
+			'id' => NULL,
+			//site id for museum
+			'current-day' => false,
+			//events by current day
+			'current-month' => false,
+			//events by current month
+			'date-range' => false,
+			//events by range
+			'range-start' => NULL,
+			//start range
+			'range-end' => NULL,
+			//end range
 			'limit' => NULL // num of events to display
 		),
 		$atts
@@ -81,32 +87,32 @@ function dca_events_plugin($atts = [])
 		$currentDate = date('Y-m-d');
 
 		//format for url
-		$_API_URL .= "&start_date=". $currentDate ."&end_date=". $currentDate;
-		
+		$_API_URL .= "&start_date=" . $currentDate . "&end_date=" . $currentDate;
+
 	} else if ($_CURR_MONTH_OPT == true) {
 		//get_events_by_current_month
 		//get first and last day of this month as range
 		$currentDate = date('Y-m-d');
 		$first = date('Y-m-01', strtotime($currentDate));
 		$last = date('Y-m-t', strtotime($currentDate));
-		
+
 		//format for url
-		$_API_URL .= "&start_date=". $first ."&end_date=". $last;
+		$_API_URL .= "&start_date=" . $first . "&end_date=" . $last;
 
 	} else if ($_DATE_RANGE_OPT == true) {
 		//get_events_by_range
 		$start_date = $_DATE_RANGE_START;
 		$end_date = $_DATE_RANGE_END;
-		
+
 		//format for url
-		$_API_URL .= "&start_date=". $start_date ."&end_date=". $end_date;
+		$_API_URL .= "&start_date=" . $start_date . "&end_date=" . $end_date;
 
 	} else {
 		//default - get_events_by_current_month
 		$currentDate = date('Y-m-d');
 		$first = date('Y-m-01', strtotime($currentDate));
 		$last = date('Y-m-t', strtotime($currentDate));
-		$_API_URL .= "&start_date=". $first ."&end_date=". $last;
+		$_API_URL .= "&start_date=" . $first . "&end_date=" . $last;
 	}
 
 	//set venue in api url
@@ -117,37 +123,41 @@ function dca_events_plugin($atts = [])
 	// USE LATER with dropdown: $get_feed = file_get_contents($feed_setting + $_API_URL);
 
 	// Testing [dca_events date-range='true' range-start=2023-06-26 range-end=2023-06-29]
-
 	$json_data = file_get_contents($_API_URL);
 	$response_data = json_decode($json_data);
 	$event_data = $response_data;
 
-	// return results (html output)
-	// start div box
-	echo("<script>console.log('PHP: " . $_API_URL . "');</script>");
-	
-	$output = '<div class="dca-events">';
+	if ($event_data !== null) {
+		// return results (html output)
+		// start div box
+		echo ("<script>console.log('PHP: " . $_API_URL . "');</script>");
 
-	foreach ($event_data as $events) {
-			$output = '<div class="dca-event">';
-			$output .= "<b>" . "Venue: " . $events->venue->url->venue. "</b>" . "<br> " ;
+
+		foreach ($event_data as $events) {
+			print_r($events->venue->url->venue);
+
+			$output .= '<div class="dca-event">';
+			$output .= "<b>" . "Venue: " . $events->venue->url->venue . "</b>" . "<br> ";
 			$output .= "<h5>" . "Title: " . $events->title . "</h5>" . "<br> ";
 			$output .= "<p>" . "Description: " . $events->venue->url->description . "</p>" . "<br>";
-			$output = '</div>';
+			$output .= '</div>';
+		}
+
+
+	} else {
+		$output .= "<b>" . "Unknown Error" . "</b>" . "<br> ";
+
+
 	}
-	// end div box
-	$output .= '</div>';
+
 
 	// return output
 	return $output;
 
 }
 
-
-
 //register shortcode
 add_shortcode('dca_events', 'dca_events_plugin');
-
 
 /*
  *
@@ -157,7 +167,6 @@ add_shortcode('dca_events', 'dca_events_plugin');
 
 
 // @Anita Your plugin settings page code goes here:
-
 class RSSEventsPage
 {
 	private $rss_events_page_options;
@@ -171,10 +180,14 @@ class RSSEventsPage
 	public function rss_events_page_add_plugin_page()
 	{
 		add_options_page(
-			'DCA Events Plugin', // page_title
-			'DCA Events Plugin', // menu_title
-			'manage_options', // capability
-			'rss-events-page', // menu_slug
+			'DCA Events Plugin',
+			// page_title
+			'DCA Events Plugin',
+			// menu_title
+			'manage_options',
+			// capability
+			'rss-events-page',
+			// menu_slug
 			array($this, 'rss_events_page_create_admin_page') // function
 		);
 	}
@@ -183,26 +196,26 @@ class RSSEventsPage
 	{
 		$this->rss_events_page_options = get_option('rss_events_page_option_name'); ?>
 
-												<div class="wrap">
-													<h2>DCA Events Plugin</h2>
-													<h3>Shortcode Options</h3>
-														<ul>
-														<li>&emsp; per_page => </li>
-														<li>&emsp; start_date => </li>
-														<li>&emsp; end_date => </li>
-														<li>&emsp; venue => </li>
-														</ul> 
-													<?php settings_errors(); ?>
+																				<div class="wrap">
+																					<h2>DCA Events Plugin</h2>
+																					<h3>Shortcode Options</h3>
+																						<ul>
+																						<li>&emsp; limit => </li>
+																						<li>&emsp; start_date => </li>
+																						<li>&emsp; end_date => </li>
+																						<li>&emsp; venue => </li>
+																						</ul> 
+																					<?php settings_errors(); ?>
 
-													<form method="post" action="options.php">
-														<?php
-														settings_fields('rss_events_page_option_group');
-														do_settings_sections('rss-events-page-admin');
-														submit_button();
-														?>
-													</form>
-												</div>
-						<?php }
+																					<form method="post" action="options.php">
+																						<?php
+																						settings_fields('rss_events_page_option_group');
+																						do_settings_sections('rss-events-page-admin');
+																						submit_button();
+																						?>
+																					</form>
+																				</div>
+										<?php }
 
 	public function rss_events_page_page_init()
 	{
@@ -225,10 +238,14 @@ class RSSEventsPage
 		);
 
 		add_settings_field(
-			'rss_feed_0', // id
-			'RSS Feed', // title
-			array($this, 'rss_feed_0_callback'), // callback
-			'rss-events-page-admin', // page
+			'rss_feed_0',
+			// id
+			'RSS Feed',
+			// title
+			array($this, 'rss_feed_0_callback'),
+			// callback
+			'rss-events-page-admin',
+			// page
 			'rss_events_page_setting_section' // section
 		);
 	}
