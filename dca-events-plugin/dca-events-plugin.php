50eb8523467b3caf44f7e113e59de48b1decb65d
Rianne <9632612+littlepandabear@users.noreply.github.com>
2023-07-06 16:57:16 -0600

    Update dca-events-plugin.php

--- dca-events-plugin/dca-events-plugin.php
+++ dca-events-plugin/dca-events-plugin.php
@@ -29,13 +29,8 @@ function dca_events_plugin($atts = [])
 	//gets shortcode vals
 	$atts = shortcode_atts(
 		array(
-<<<<<<< HEAD
 			'site' => NULL, //site id for museum
 			'today' => false, //events by current day
-=======
-			'id' => NULL, //site id for museum
-			'current-day' => false, //events by current day
->>>>>>> f3f6928550b385825c67caa2843a11f9c1a76711
 			'current-month' => false, //events by current month
 			'date-range' => false, //events by range
 			'range-start' => NULL, //start range
@@ -65,14 +60,10 @@ function dca_events_plugin($atts = [])
 	// set default limit if null - else typecast shortcode val
 	$_LIMIT_OPT = ($atts['limit'] == NULL) ? 10 : intval($atts['limit']);
 
-<<<<<<< HEAD
+
 	//get events from site option else get default in settings menu
 	$_SITE_ID = ($atts['site'] == NULL) ? intval($atts['site']) :  get_option('dca_events_plugin_option_name')['venue_id_0'] ;
 	
-=======
-	//get events from set default in settings menu
-	$_SITE_ID = get_option('dca_events_plugin_option_name')['venue_id_0'];
->>>>>>> f3f6928550b385825c67caa2843a11f9c1a76711
 	// TODO: should I set a default for all events in the dropdown?
 	//       if so how do I do that its just with no ?venues in the api right?
 
@@ -122,40 +113,26 @@ function dca_events_plugin($atts = [])
 
 	//set venue in api url
 	$_API_URL .= "&venues=" . $_SITE_ID;
-<<<<<<< HEAD
-	
-=======
->>>>>>> f3f6928550b385825c67caa2843a11f9c1a76711
 
 	//make API request
 	$json_data = file_get_contents($_API_URL);
 	$response_data = json_decode($json_data);
-<<<<<<< HEAD
 	$event_data = $response_data->events;
 	
 	
 	echo ("<script>console.log('URL: " . $_API_URL . "');</script>");
 	//echo var_dump($event_data);
 
-	if ($event_data == null) {
-		echo json_last_error() . "<br>";
-=======
-	$event_data = $response_data;
-	echo ("<script>console.log('PHP: " . $_API_URL . "');</script>");
 
 	if ($event_data == null) {
 		echo json_last_error() . "<br>";
   		echo $event_data; 
->>>>>>> f3f6928550b385825c67caa2843a11f9c1a76711
 
 	} else {
 
 		// return results (html output)
 		foreach ($event_data as $events) {
-<<<<<<< HEAD
-			
-=======
->>>>>>> f3f6928550b385825c67caa2843a11f9c1a76711
+
 			$output .= '<div class="dca-event">';
 			// title, description, event dates, venue name, address
 			$output .= "<h5>" . "Title: " . $events->title . "</h5>" . "<br> ";
