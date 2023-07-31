4d30fa01504884d98cd63405eb590089c72cb232
Rianne <9632612+littlepandabear@users.noreply.github.com>
2023-07-31 10:33:50 -0600

    update

--- dca-events-plugin/dca-events-plugin.php
+++ dca-events-plugin/dca-events-plugin.php
@@ -41,8 +41,20 @@ function formatEventTime($date)
 // Function for an API request
 function api_request($url)
 {
-	$response = file_get_contents($url);
+	
+	$arrContextOptions = array(
+	      "ssl" => array(
+	        "verify_peer" => false,
+	        "verify_peer_name" => false,
+	      )
+	);  
+  
+	$context = stream_context_create($arrContextOptions);
+	$response = file_get_contents($url,false,$context);
+	
+	
 	return json_decode($response);
+	
 }
 
 
@@ -130,8 +142,14 @@ function dca_events_plugin($atts = [])
 		$_API_URL .= "&start_date=" . $first . "&end_date=" . $last;
 	}
 
-	// Set venue in API URL
-	$_API_URL .= "&venue=" . $_SITE_ID;
+
+	if ($_SITE_ID != NULL){
+		
+		// Set venue in API URL
+		$_API_URL .= "&venue=" . $_SITE_ID;
+		
+	}
+	
 
 	// Make an API request
 	$response_data = api_request($_API_URL);
@@ -201,8 +219,11 @@ function addBootStrap()
 	wp_enqueue_style("bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css");
 	wp_enqueue_script("bootstrapJS", "https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js", array('jquery'),true);
 }
-// Adding a hook for bootstrap
+// Add bootstrap to templates
 add_action("wp_enqueue_scripts", "addBootStrap");
+// Add bootstrap to admin settings
+add_action( 'admin_init', 'addBootStrap' );
+
 
 /*
  *
@@ -244,43 +265,62 @@ class DCAEventsPlugin
 	public function dca_events_plugin_create_admin_page()
 	{
 		$this->dca_events_plugin_options = get_option('dca_events_plugin_option_name'); ?>
-										<div class="wrap">
-											<h2>DCA Events Plugin</h2>
-											<h3>How to use this Plug-in</h3>
-											<p>Using shortcode you can pull in events by site id, dates, and number of events.</p>
-											
-											<h3>Options Avaliable</h3>
-											<table width="1000px" border="1px">
-												<tr> <th>NAME</th> <th>DESCRIPTION</th> <th>FORMAT</th> <th>OPTION</th> </tr>
-												<tr> <td>site</td> <td>this option you select on this page under the Settings Section</td> <td>Drop down menu</td> <td>list of sites</td> </tr>
-												<tr> <td>today</td> <td>returns events by current day</td> <td>current-day= </td> <td>TRUE or FALSE</td> </tr>
-												<tr> <td>current-month</td> <td>returns events by current month</td> <td>current-month= </td> <td>TRUE or FALSE</td> </tr>
-												<tr> <td>date-range</td> <td>return events by range</td> <td>date-range= </td> <td>TRUE or FALSE</td> </tr>
-												<tr> <td>range-start</td> <td>used only with date-range, define a specific start range</td> <td>only in YYYY-MM-DD</td> <td>any dates</td> </tr>
-												<tr> <td>range-end</td> <td>used only with date-range, define a specific end range</td> <td>only in YYYY-MM-DD</td> <td>any dates</td> </tr>
-												<tr> <td>limit</td> <td>returns the number of events to display</td> <td>limit=</td> <td>any integer value</td> </tr>
-											</table>
-
-											<h3>Instructions</h3>
-											<p>1. Make sure plug-in is activated.</p>
-											<p>2. Select the site you want to pull events from under the Settings section below.</p>
-											<p>3. Go to the page you want events to be displayed.</p>
-											<p>4. Using the shortcode format type in your selections: [dca-events **options ]</p>
-											
-											<h3>Examples</h3>
-											<p>[dca_events limit=7 current-month='true'] will return 7 events for the current month</p>
-											<p>[dca_events limit=4 current-day='true'] will return 4 events for the current day</p>
-											<p>[dca_events limit=2 date-range='true' range-start=2023-07-19 range-end=2023-07-23] will return 2 events that are happening July 19, 2023 to July 23, 2023 </p>
-											<?php settings_errors(); ?>
-											<form method="post" action="options.php">
-												<?php
-												settings_fields('dca_events_plugin_option_group');
-												do_settings_sections('dca-events-plugin-admin');
-												submit_button();
-												?>
-											</form>
-										</div>
-					<?php }
+		
+		<div class="container">
+			<h1 class="display-1 mb-2">DCA Events Plugin</h1>
+			
+			<h3>How to use Shortcode</h3>
+			<p>Using shortcode you can pull in events by site id, dates, and number of events.</p>									
+			
+			<h4>Instructions</h4>
+			<p>- Create a post or page.</p>
+			<p>- Using the shortcode format type in your selections: [dca_events **options ]</p>
+			
+			
+			<b>Examples</b>
+			<p>[dca_events limit=7 current-month='true'] will return 7 events for the current month</p>
+			<p>[dca_events limit=4 current-day='true'] will return 4 events for the current day</p>
+			<p>[dca_events limit=2 date-range='true' range-start=2023-07-19 range-end=2023-07-23] will return 2 events that are happening July 19, 2023 to July 23, 2023 </p>
+			
+			<b>Shortcode Options Avaliable</b>
+			
+			<table class="table border"  >
+				<tr> <th>NAME</th> <th>DESCRIPTION</th> <th>FORMAT</th> <th>OPTION</th> </tr>
+				<tr> <td>site</td> <td>this option you select on this page under the Settings Section</td> <td>Drop down menu</td> <td>list of sites</td> </tr>
+				<tr> <td>today</td> <td>returns events by current day</td> <td>current-day= </td> <td>TRUE or FALSE</td> </tr>
+				<tr> <td>current-month</td> <td>returns events by current month</td> <td>current-month= </td> <td>TRUE or FALSE</td> </tr>
+				<tr> <td>date-range</td> <td>return events by range</td> <td>date-range= </td> <td>TRUE or FALSE</td> </tr>
+				<tr> <td>range-start</td> <td>used only with date-range, define a specific start range</td> <td>only in YYYY-MM-DD</td> <td>any dates</td> </tr>
+				<tr> <td>range-end</td> <td>used only with date-range, define a specific end range</td> <td>only in YYYY-MM-DD</td> <td>any dates</td> </tr>
+				<tr> <td>limit</td> <td>returns the number of events to display</td> <td>limit=</td> <td>any integer value</td> </tr>
+			</table>
+			
+			
+			
+			
+				
+			<form method="post" action="options.php">
+				
+				
+				<?php
+				
+				
+					settings_fields('dca_events_plugin_option_group');
+					
+					do_settings_sections('dca-events-plugin-admin');
+					
+					
+					submit_button();
+					?>
+			</form>
+			
+			<p>View <?php echo "<a href='".site_url() . "/events"."'>". site_url() . "/events"."</a>"; ?></p>
+			
+			
+			
+		
+		</div>
+	<?php }
 
 	// Function for intializing fields
 	public function dca_events_plugin_page_init()
@@ -299,7 +339,7 @@ class DCAEventsPlugin
 			// id
 			'dca_events_plugin_setting_section',
 			// title
-			'Settings',
+			'Events Page Settings',
 			// callback
 			array($this, 'dca_events_plugin_section_info'),
 			// page
@@ -334,6 +374,8 @@ class DCAEventsPlugin
 
 	public function dca_events_plugin_section_info()
 	{ // left blank intentionally
+		
+
 	}
 
 	// Callback function for retreiving venues
@@ -344,7 +386,18 @@ class DCAEventsPlugin
 		$response_data = api_request($url);
 		// for loop for looping through all the venues and outputting a dropdown menu
 		?> 				
+		
 				<select name="dca_events_plugin_option_name[venue_id_0]" id="venue_id_0"> ?>
+					
+					<?php
+					
+					 $all = (isset($this->dca_events_plugin_options['venue_id_0']) && $this->dca_events_plugin_options['venue_id_0'] == NULL )? 'selected' : '';
+					 
+					?>
+					
+					<option value="" <?php echo $all; ?> > 
+						All Sites
+					</option>
 				<?php foreach ($response_data->venues as $venue) { ?>
 		
 									<?php
@@ -363,8 +416,6 @@ class DCAEventsPlugin
 // Plugin will only show if you are logged in as the administrator
 if (is_admin())
 	$dca_events_plugin = new DCAEventsPlugin();
-
-
 
 
 
